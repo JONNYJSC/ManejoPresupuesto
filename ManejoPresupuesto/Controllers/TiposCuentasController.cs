240c56872502f732a8e4cc615a40bd9e5524425a
JONNYJSC <jjonath21@gmail.com>
2023-04-20 10:52:13 -0600

    Haciendo un post con el FETCH API

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -120,6 +120,12 @@ namespace ManejoPresupuesto.Controllers
 
             return Json(true);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Ordenar([FromBody] int[] ids)
+        {
+            return Ok();
+        }
         
     }
 }
