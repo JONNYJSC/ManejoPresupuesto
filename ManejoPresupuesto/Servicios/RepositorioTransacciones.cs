1e74074688d418f1235cae6f28d560932789d32b
JONNYJSC <jjonath21@gmail.com>
2023-05-20 16:23:54 -0600

    Creando Transacciones

---
+++ ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioTransacciones
+    {
+        Task Crear(Transaccion transaccion);
+    }
+    public class RepositorioTransacciones : IRepositorioTransacciones
+    {
+        private readonly string connectionString;
+        public RepositorioTransacciones(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(Transaccion transaccion)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QueryFirstAsync<int>("SP_Transacciones_Insertar",
+                new
+                {
+                    transaccion.UsuarioId,
+                    transaccion.FechaTransaccion,
+                    transaccion.Monto,
+                    transaccion.CategoriaId,
+                    transaccion.CuentaId,
+                    transaccion.Nota
+                },
+            commandType: CommandType.StoredProcedure);
+
+            transaccion.Id = id;
+        }
+    }
+}
